9ea8f6f13518ff68f8562778474209920ce57ccd
khasbi <121207980+khasbi@users.noreply.github.com>
2023-03-24 10:11:36 +0700

    Update arduino.ino

--- aka_robot_controller/firmware/src/arduino.ino
+++ aka_robot_controller/firmware/src/arduino.ino
@@ -12,6 +12,7 @@
 #include <Kinematics.h>
 #include "base_config.h"
 #include "Motor.h"
+// #include "run_led.h"
 // #include "Encoder.h"
 #include "Arduino.h"
 #include <ros.h>
@@ -19,9 +20,18 @@
 #include <geometry_msgs/Vector3Stamped.h>
 #include <geometry_msgs/Twist.h>
 #include <ros/time.h>
+#include <FastLED.h>
 
 #define COMMAND_RATE 20 //hz
 #define DEBUG_RATE 5
+
+#define frontLed_pin 22
+#define rearLed_pin 24
+#define frontLED 15
+#define rearLED 39
+
+CRGB FLeds[frontLED];
+CRGB RLeds[rearLED];
 
 Kinematics kinematics(MAX_RPM, WHEEL_DIAMETER, WHEELS_DISTANCE, PWM_BITS);
 
@@ -70,17 +80,12 @@ void setup()
 {
   nh.initNode();
   nh.getHardware()->setBaud(57600);
-//  nh.subscribe(pid_sub);
   nh.subscribe(cmd_sub);
   nh.advertise(raw_vel);
-//  nh.advertise(raw_imu_pub);
 
-  // while (!nh.connected())
-  // {
-  //   nh.spinOnce();
-  // }
-  // nh.loginfo("ROBOT CONNECTED");
-  //delay(1);
+  FastLED.addLeds<WS2812,frontLed_pin, GRB>(FLeds, frontLED);
+  FastLED.addLeds<WS2812,rearLed_pin, GRB>(RLeds, rearLED);
+  // FastLED.setBrightness(200);
 
   pinMode(MOTOR_LEFT_ENC_A, INPUT_PULLUP);
   pinMode(MOTOR_LEFT_ENC_B, INPUT_PULLUP);
@@ -98,7 +103,7 @@ void loop()
   static unsigned long prev_control_time = 0;
   static unsigned long prev_debug_time = 0;
   currentMillis = millis();
-  
+
   if(currentMillis - previousMillis >= 1000)
   {
       previousMillis = currentMillis;
@@ -124,11 +129,12 @@ void loop()
     stopBase();
   }
 
-  if(0)
+  if(1)
   {
     if((millis() - prev_debug_time) >= (1000 / DEBUG_RATE))
     {
-      printDebug();
+      // printDebug();
+      // runLed();
       prev_debug_time = millis();
     }
   }
@@ -145,7 +151,29 @@ void moveBase()
   int left_rpm = req_rpm.motor_left;
   int right_rpm = req_rpm.motor_right;
 
-  char cek[50];
+  // if (left_rpm > 0 && right_rpm > 0){runLed_forward();}
+  // else if (left_rpm < 0 && right_rpm > 0){runLed_left();}
+  // else if (left_rpm > 0 && right_rpm < 0){runLed_right();}
+
+  // get actual rpm from encoder data
+  Kinematics::velocities current_vel;
+  current_vel = kinematics.getVel(act_rpm_left, act_rpm_right);
+
+  char buffL[50];                                                           // print act rpm
+  char msgL[50];
+  char buffR[50];
+  char msgR[50];
+
+  dtostrf(act_rpm_left, 6, 2, buffL);
+  sprintf(msgL, "before left: %s", buffL);
+  nh.loginfo(msgL);
+
+  dtostrf(act_rpm_right, 6, 2, buffR);
+  sprintf(msgR, "before right: %s", buffR);
+  nh.loginfo(msgR);
+
+  
+  char cek[50];                                                             // print req rpm
 
   sprintf(cek, "req left rpm = %d", left_rpm);
   nh.loginfo(cek);
@@ -159,32 +187,34 @@ void moveBase()
   int pwmLeft = map(((int)left_rpm), 0, MAX_RPM, 0, 255);
   int pwmRight = map(((int)right_rpm), 0, MAX_RPM, 0, 255);
 
-  char pwm[50];
+  char pwm[50];                                                             // print req pwm
 
   sprintf(pwm, "left pwm = %d", pwmLeft);
   nh.loginfo(pwm);
   sprintf(pwm, "right pwm = %d", pwmRight);
   nh.loginfo(pwm);
 
-  Kinematics::velocities current_vel;
-  current_vel = kinematics.getVel(act_rpm_left, act_rpm_right);
+  // get actual rpm from encoder data
+  // Kinematics::velocities current_vel;
+  // current_vel = kinematics.getVel(act_rpm_left, act_rpm_right);
 
-  //pass velocities to publisher object
+  // pass velocities to publisher object
+  // raw_vel_msg.linear.x = current_vel.linear_x;
   raw_vel_msg.linear.x = current_vel.linear_x;
   raw_vel_msg.angular.z = current_vel.angular_z;
   
-  char buffL[50];
-  char msgL[50];
-  char buffR[50];
-  char msgR[50];
+  char buffLe[50];                                                           // print act rpm
+  char msgLe[50];
+  char buffRe[50];
+  char msgRe[50];
 
-  dtostrf(act_rpm_left, 6, 2, buffL);
-  sprintf(msgL, "left: %s", buffL);
-  nh.loginfo(msgL);
+  dtostrf(act_rpm_left, 6, 2, buffLe);
+  sprintf(msgLe, "after left: %s", buffLe);
+  nh.loginfo(msgLe);
 
-  dtostrf(act_rpm_right, 6, 2, buffR);
-  sprintf(msgR, "right: %s", buffR);
-  nh.loginfo(msgR);
+  dtostrf(act_rpm_right, 6, 2, buffRe);
+  sprintf(msgRe, "after right: %s", buffRe);
+  nh.loginfo(msgRe);
 
   //publih raw_vel_msg
   raw_vel.publish(&raw_vel_msg);  
@@ -195,6 +225,13 @@ void stopBase()
   req_linear_vel_x = 0;
   req_linear_vel_y = 0;
   req_angular_vel_z = 0;
+  // runLed_steady();  
+
+  // raw_vel_msg.linear.x = req_linear_vel_x;
+  // raw_vel_msg.angular.z = req_angular_vel_z;
+
+  // //publish raw_vel_msg
+  // raw_vel.publish(&raw_vel_msg);  
 }
 
 // int mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
@@ -217,6 +254,362 @@ void printDebug()
   sprintf(msgR, "right: %s", buffR);
   nh.loginfo(msgR);
 }
+
+void runLed_forward()
+{
+  for (int i = 0; i <= (rearLED - 1) / 2; i++)
+  {
+    for (int j = 0; j <= 6; j++)
+    {
+      RLeds[i + j] = CRGB (0, 0, 0);
+      RLeds[((rearLED - 1) - i) - j] = CRGB (0, 0, 0);
+    }
+    if (i > 6)
+    {
+      RLeds[i - 7] = CRGB (0, 0, 0); 
+      RLeds[((rearLED - 1) - i) + 7] = CRGB (0, 0, 0);
+    }
+    RLeds[i] = CRGB (0, 200, 0);
+    RLeds[(rearLED - 1) - i] = CRGB (0, 200, 0);
+
+    for (int k = 0; k <= frontLED - 1; k++)
+    {
+      FLeds[k] = CRGB (0, 50, 0);
+    }
+    FastLED.show();
+    FastLED.setBrightness(200);
+    delay(30);
+  }
+}
+
+void runLed_right()
+{
+  for (int i = 0; i < frontLED; i++)   // front
+  {
+    if (i < 3)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else if (i > frontLED - 4)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+  }
+  for (int i = 0; i < rearLED - 1; i++)    // rear
+  {
+    RLeds[i] = CRGB (0, 100, 0);
+  }
+  FastLED.show();
+  FastLED.setBrightness(200);
+  delay(200);
+
+  for (int i = 0; i < frontLED; i++)   // front
+  {
+    if (i < 3)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else if (i > frontLED - 4)
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+    else
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+  }
+  for (int i = 0; i < rearLED ; i++)    // rear
+  {
+    if (i > 26)
+    {
+      RLeds[i] = CRGB (0, 0, 0);
+    }
+    else
+    {
+      RLeds[i] = CRGB (0, 100, 0);
+    }   
+  }  
+  FastLED.show();
+  delay(200);
+}
+
+void runLed_left()
+{
+  for (int i = 0; i < frontLED; i++)   // front
+  {
+    if (i < 3)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else if (i > frontLED - 4)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+  }
+  for (int i = 0; i < rearLED - 1; i++)    // rear
+  {
+    RLeds[i] = CRGB (0, 100, 0);
+  }
+  FastLED.show();
+  FastLED.setBrightness(200);
+  delay(200);
+
+  for (int i = 0; i < frontLED; i++)   // front
+  {
+    if (i < 3)
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+    else if (i > frontLED - 4)
+    {
+      FLeds[i] = CRGB (0, 50, 0);
+    }
+    else
+    {
+      FLeds[i] = CRGB (0, 0, 0);
+    }
+  }
+  for (int i = 0; i < rearLED ; i++)    // rear
+  {
+    if (i < 12)
+    {
+      RLeds[i] = CRGB (0, 0, 0);
+    }
+    else
+    {
+      RLeds[i] = CRGB (0, 100, 0);
+    }   
+  }  
+  FastLED.show();
+  delay(200);
+}
+
+void runLed_steady()
+{
+  for (int i = 10; i <= 200; i = i + 10)
+  {
+    for (int j = 0; j <= rearLED - 1; j ++)
+    {
+      RLeds[j] = CRGB (0, 100, 0);
+    }
+    for (int k = 0; k <= frontLED - 1; k++)
+    {
+      FLeds[k] = CRGB (0, 50, 0);
+    }
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(60);    
+  }
+  for (int i = 200; i >= 10; i = i - 10)
+  {
+    for (int j = 0; j <= rearLED - 1; j ++)
+    {
+      RLeds[j] = CRGB (0, 100, 0);
+    }
+    for (int k = 0; k <= frontLED - 1; k++)
+    {
+      FLeds[k] = CRGB (0, 50, 0);
+    }
+    FastLED.setBrightness(i);
+    FastLED.show();
+    delay(60);    
+  }
+}
+
+// void runLed()
+// {
+//   //get required rpm for each motor based on required velocities
+//   Kinematics::output readReq = kinematics.getRPM(req_linear_vel_x, req_linear_vel_y, req_angular_vel_z);
+
+//   //get the current rpm of each motor
+//   int leftReq = readReq.motor_left;
+//   int rightReq = readReq.motor_right;
+
+//   char cek[50];                                                             // print req rpm
+
+//   sprintf(cek, "req left led = %d", leftReq);
+//   nh.loginfo(cek);
+//   sprintf(cek, "req right led = %d", rightReq);
+//   nh.loginfo(cek);
+
+
+//   if (leftReq > 0 && rightReq > 0)             // front and rear leds in forward movement
+//   {
+//     for (int i = 0; i <= (rearLED - 1) / 2; i++)
+//     {
+//       for (int j = 0; j <= 6; j++)
+//       {
+//         RLeds[i + j] = CRGB (0, 0, 0);
+//         RLeds[((rearLED - 1) - i) - j] = CRGB (0, 0, 0);
+//       }
+//       if (i > 6)
+//       {
+//         RLeds[i - 7] = CRGB (0, 0, 0); 
+//         RLeds[((rearLED - 1) - i) + 7] = CRGB (0, 0, 0);
+//       }
+//       RLeds[i] = CRGB (0, 200, 0);
+//       RLeds[(rearLED - 1) - i] = CRGB (0, 200, 0);
+
+//       for (int k = 0; k <= frontLED - 1; k++)
+//       {
+//         FLeds[k] = CRGB (0, 50, 0);
+//       }
+//       FastLED.show();
+//       FastLED.setBrightness(200);
+//       delay(30);
+//     }
+//   }
+
+//   if (leftReq < 0 && rightReq > 0)            // front and rear leds when turn left
+//   {
+//     for (int i = 0; i < frontLED; i++)   // front
+//     {
+//       if (i < 3)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else if (i > frontLED - 4)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//     }
+//     for (int i = 0; i < rearLED - 1; i++)    // rear
+//     {
+//       RLeds[i] = CRGB (0, 100, 0);
+//     }
+//     FastLED.show();
+//     FastLED.setBrightness(200);
+//     delay(200);
+
+//     for (int i = 0; i < frontLED; i++)   // front
+//     {
+//       if (i < 3)
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//       else if (i > frontLED - 4)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//     }
+//     for (int i = 0; i < rearLED ; i++)    // rear
+//     {
+//       if (i < 12)
+//       {
+//         RLeds[i] = CRGB (0, 0, 0);
+//       }
+//       else
+//       {
+//         RLeds[i] = CRGB (0, 100, 0);
+//       }   
+//     }  
+//     FastLED.show();
+//     delay(200);
+//   }
+  
+//   if (leftReq > 0 && rightReq < 0)             // front and rear leds when turn right
+//   {
+//     for (int i = 0; i < frontLED; i++)   // front
+//     {
+//       if (i < 3)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else if (i > frontLED - 4)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//     }
+//     for (int i = 0; i < rearLED - 1; i++)    // rear
+//     {
+//       RLeds[i] = CRGB (0, 100, 0);
+//     }
+//     FastLED.show();
+//     FastLED.setBrightness(200);
+//     delay(200);
+
+//     for (int i = 0; i < frontLED; i++)   // front
+//     {
+//       if (i < 3)
+//       {
+//         FLeds[i] = CRGB (0, 50, 0);
+//       }
+//       else if (i > frontLED - 4)
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//       else
+//       {
+//         FLeds[i] = CRGB (0, 0, 0);
+//       }
+//     }
+//     for (int i = 0; i < rearLED ; i++)    // rear
+//     {
+//       if (i > 26)
+//       {
+//         RLeds[i] = CRGB (0, 0, 0);
+//       }
+//       else
+//       {
+//         RLeds[i] = CRGB (0, 100, 0);
+//       }   
+//     }  
+//     FastLED.show();
+//     delay(200);
+//   }
+
+//   if (leftReq == 0 && rightReq == 0)                       // front and rear leds in steady
+//   {
+//     for (int i = 10; i <= 200; i = i + 10)
+//     {
+//       for (int j = 0; j <= rearLED - 1; j ++)
+//       {
+//         RLeds[j] = CRGB (0, 100, 0);
+//       }
+//       for (int k = 0; k <= frontLED - 1; k++)
+//       {
+//         FLeds[k] = CRGB (0, 50, 0);
+//       }
+//       FastLED.setBrightness(i);
+//       FastLED.show();
+//       delay(60);    
+//     }
+//     for (int i = 200; i >= 10; i = i - 10)
+//     {
+//       for (int j = 0; j <= rearLED - 1; j ++)
+//       {
+//         RLeds[j] = CRGB (0, 100, 0);
+//       }
+//       for (int k = 0; k <= frontLED - 1; k++)
+//       {
+//         FLeds[k] = CRGB (0, 50, 0);
+//       }
+//       FastLED.setBrightness(i);
+//       FastLED.show();
+//       delay(60);    
+//     }
+//   }
+// }
 
 void enc_left_A()
 {
